dc48791044b476b4810a463ed558c3e8b5879312
Hugo Kemme <hjr.kemme@student.han.nl>
2024-10-12 16:07:52 +0200

    project setup

---
+++ MailService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("This is an unstable mail service!");
+        
+        while (true)
+        {
+            await Task.Delay(5000);  // Simulate doing work every second
+            Console.WriteLine("Still running...");
+        }
+    }
+}
